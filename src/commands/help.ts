3631468e2258383b129746427b236b4f7408a47c
Ya snova s vami <kirofint@gmail.com>
2021-03-24 17:42:49 +0300

    [FIX] package name in lower case

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,4 +1,4 @@
-import { Telegraf, Context } from 'Telegraf'
+import { Telegraf, Context } from 'telegraf'
 
 export function commandRun (bot: Telegraf<Context>) {
   bot.command(['help', 'start'], ctx => {
